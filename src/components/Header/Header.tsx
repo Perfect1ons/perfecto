533736e3db24a5ac8490c2c6f9999d2df9c938ba
Abdulaziz <meisanaziz06@gmail.com>
2024-04-17 17:06:04 +0600

    some fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,29 +2,41 @@ import cn from 'clsx'
 import styles from './style.module.scss'
 import Logo from '../Logo/Logo';
 import HeaderNav from './HeaderNav/HeaderNav';
+import { SearchIcon, SearchIconWhite } from '../../../public/Icons/Icons';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={cn(styles.header__container, "container")}>
-        
         <div className={styles.logo}>
-            <Logo/>
+          <Logo />
         </div>
 
-        <div className={styles.catalog}>
-
-        </div>
+        <Link className={styles.catalog} href={'/catalog'}>
+          Каталог
+        </Link>
 
         <div className={styles.search}>
-
+          <input
+            type="text"
+            id="searchInput"
+            className={styles.search__input}
+          />
+          <label htmlFor="searchInput" className={styles.search__icon}>
+            <SearchIcon />
+          </label>
         </div>
 
         <div className={styles.header__nav}>
-            <HeaderNav/>
+          <HeaderNav />
         </div>
 
+        <div className={styles.search__white}>
+          <SearchIconWhite/>
+        </div>
       </div>
     </header>
   );
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from './style.module.scss'
 
 const Logo = () => {
   return (
     <Link href={"/"}>
       <Image
+        className={styles.logo}
         src={"/img/logo.svg"}
         width={211}
         height={83}
