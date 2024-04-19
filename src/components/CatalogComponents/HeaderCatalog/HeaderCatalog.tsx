c38d8d2b5812e7708b90819d04e7b680b9aba0a7
Эндарбек <endarashiraliev@gmail.com>
2024-04-19 17:05:16 +0600

    add subcatalog

--- src/components/CatalogComponents/HeaderCatalog/HeaderCatalog.tsx
+++ src/components/CatalogComponents/HeaderCatalog/HeaderCatalog.tsx
@@ -1,34 +1,20 @@
-"use client";
 import { ICatalogHome } from "@/types/catalogsHome";
 import React, { useState } from "react";
 import styles from "./style.module.scss";
-import { getSubCatalogs } from "@/api/requests";
-import { subCatalog } from "@/types/subCatalog";
 interface ICatalogProps {
   catalog: ICatalogHome[];
 }
 const HeaderCatalog = ({ catalog }: ICatalogProps) => {
-  const [subCatalogs, setSubCatalogs] = useState<subCatalog>();
-
-  const handleMouseEnter = (id: number) => {
-    getSubCatalogs(id).then((data) => {
-      setSubCatalogs(data);
-    });
-  };
-
   return (
-    <div>
-      {catalog.map((catalog) => (
-        <div key={catalog.id} className={styles.catalogs}>
-          <h2
-            className={styles.catalogs__h2}
-            onClick={() => handleMouseEnter(catalog.id)}
-          >
+    <div className={styles.catalogs}>
+      <div>
+        {catalog.map((catalog) => (
+          <h2 className={styles.catalogs__h2} key={catalog.id}>
             {catalog.name}
           </h2>
-        </div>
-      ))}
-      {subCatalogs?.category[0].name}
+        ))}
+      </div>
+      <div></div>
     </div>
   );
 };
