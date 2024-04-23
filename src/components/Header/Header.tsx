55dcb5fbf351c2eacf3c97bc82ad8578ec0af6cb
Abdulaziz <meisanaziz06@gmail.com>
2024-04-23 17:27:34 +0600

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,20 +13,23 @@ const Header = () => {
 
         <Logo />
 
-        <Link className={styles.catalog} href={"/catalog"}>
-          Каталог
-        </Link>
+        <div className={styles.header__container_form}>
+          <Link className={styles.catalog} href={"/catalog"}>
+            Каталог
+          </Link>
+          
+          <div className={styles.search}>
+            <input
+              placeholder="Искать товары и категории"
+              type="text"
+              id="searchInput"
+              className={styles.search__input}
+            />
+            <label htmlFor="searchInput" className={styles.search__icon}>
+              <SearchIcon />
+            </label>
+          </div>
 
-        <div className={styles.search}>
-          <input
-            placeholder="Искать товары и категории"
-            type="text"
-            id="searchInput"
-            className={styles.search__input}
-          />
-          <label htmlFor="searchInput" className={styles.search__icon}>
-            <SearchIcon />
-          </label>
         </div>
 
         <div className={styles.header__nav}>
