5463fcc88d7077ba44329a97fe2c09b6a2f4d49c
Ilya <ilya.yohumming@icloud.com>
2024-04-23 12:11:37 +0600

    add lats boughts section

--- src/components/HomeComponents/TodayBoughts/TodayBoughts.tsx
+++ src/components/HomeComponents/TodayBoughts/TodayBoughts.tsx
@@ -1,62 +1,100 @@
 "use client"
-import { useState } from "react";
-import { IBoughtItem } from '@/types/lastBoughts';
-import styles from './style.module.scss';
-import cn from 'clsx';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import { IBoughtItem } from "@/types/lastBoughts";
+import Image from "next/image";
+import {
+  DeliveryIcon,
+  HearthIcon,
+  HearthIconRed,
+} from "../../../../public/Icons/Icons";
 
 interface ITodayBoughtsProps {
-    boughts: IBoughtItem[];
+  boughts: IBoughtItem[];
 }
 
 const TodayBoughts = ({ boughts }: ITodayBoughtsProps) => {
-    const imageEmpty = "https://max.kg/images/discount/empty-image.png";
-    const startUrl = "https://max.kg/nal/img/";
-    const initialVisibleItems = 10;
-    const [visibleItems, setVisibleItems] = useState(initialVisibleItems);
+  const imageEmpty = "https://max.kg/images/discount/empty-image.png";
+  const startUrl = "https://max.kg/nal/img/";
+  const initialVisibleItems = 10;
+  const [visibleItems, setVisibleItems] = useState(initialVisibleItems);
 
-    const handleShowMore = () => {
-        const newVisibleItems = visibleItems + 10;
-        setVisibleItems(Math.min(newVisibleItems, boughts.length));
-    };
+  const [likedItems, setLikedItems] = useState<{[key: string]: boolean}>({});
 
-    const showMoreButton = visibleItems < boughts.length ? (
-        <button className={styles.showMoreButton} onClick={handleShowMore}>
-            Показать еще
-        </button>
+  const handleShowMore = () => {
+    const newVisibleItems = visibleItems + 10;
+    setVisibleItems(Math.min(newVisibleItems, boughts.length));
+  };
+
+  const toggleLike = (id: number) => {
+    setLikedItems(prevState => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
+  };
+
+  const showMoreButton =
+    visibleItems < boughts.length ? (
+      <button
+        className="showMoreBtn news__buttons_showMore"
+        onClick={handleShowMore}
+      >
+        Показать еще
+      </button>
     ) : (
-        <button className={styles.showMoreButton} onClick={() => setVisibleItems(initialVisibleItems)}>
-            Скрыть
-        </button>
+      <button
+        className="showMoreBtn news__buttons_showMore"
+        onClick={() => setVisibleItems(initialVisibleItems)}
+      >
+        Скрыть
+      </button>
     );
 
-    return (
-        <section className="boughts">
-            <div className={cn(styles.boughtsContainer, "container")}>
-                {boughts.slice(0, visibleItems).map((item) => {
-                    let imageUrl = imageEmpty;
+  return (
+    <section className="boughts">
+      <div className="cardContainer container">
+        <h2 className="sections__title">Сегодня купили</h2>
+        <div className="cardItemContainer">
+          {boughts.slice(0, visibleItems).map((item) => {
+            let imageUrl = imageEmpty;
 
-                    if (item.photos && item.photos.length > 0) {
-                        const photoUrl = item.photos[0].url_part.startsWith("https://")
-                            ? `https://goods-photos.static1-sima-land.com/items/${item.art}/0/280.jpg`
-                            : `${startUrl}${item.images[0].id_post}/l_${item.photos[0].url_part}`;
-                        imageUrl = photoUrl;
-                    }
-                    
-                    return (
-                        <div key={item.id} className={styles.boughtsItem}>
-                            <Image className={styles.boughtsItemImg} src={imageUrl} width={100} height={100} alt={item.naim} />
-                            <span className={styles.boughtsItemPrice}>{item.price}</span>
-                            <p className={styles.boughtsItemName}>{item.naim}</p>
-                            <span>{item.ddos}</span>
-                        </div>
-                    );
-                })}
-                {showMoreButton}
-            </div>
-        </section>
-    );
+            if (item.photos && item.photos.length > 0) {
+              const photoUrl = item.photos[0].url_part.startsWith("https://")
+                ? `https://goods-photos.static1-sima-land.com/items/${item.art}/0/280.jpg`
+                : `${startUrl}${item.images[0].id_post}/l_${item.photos[0].url_part}`;
+              imageUrl = photoUrl;
+            }
+
+            return (
+              <div key={item.id} className="cardItem">
+                <Image
+                  className="cardItemIm"
+                  src={imageUrl}
+                  width={150}
+                  height={150}
+                  alt={item.naim}
+                />
+                <p className="cardItemPrice">{item.price} ⃀.</p>
+                <p className="cardItemName">{item.naim}</p>
+                <div className="cardItemDelivery">
+                  <DeliveryIcon />
+                  <p className="cardItemDeliveryTitle">{item.ddos}</p>
+                </div>
+                <div className="cardItemBtns">
+                  <div className="cardItemBtnsContainer">
+                    <button className="cardItemBtnsAddBucket">В корзину</button>
+                    <div className="hearthIcon" onClick={() => toggleLike(item.id)}>
+                      {likedItems[item.id] ? <HearthIconRed /> : <HearthIcon />}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {showMoreButton}
+      </div>
+    </section>
+  );
 };
 
 export default TodayBoughts;
