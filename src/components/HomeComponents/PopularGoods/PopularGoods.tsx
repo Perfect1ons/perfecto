cc9b9428ab8979def86c84f98a93f28c64b0b1d4
Ilya <ilya.yohumming@icloud.com>
2024-04-26 12:38:34 +0600

    bug fix map in popularGoods Section

--- src/components/HomeComponents/PopularGoods/PopularGoods.tsx
+++ src/components/HomeComponents/PopularGoods/PopularGoods.tsx
@@ -24,11 +24,6 @@ console.log(pageOne);
     }
   };
 
-  function formatNumber(number : number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-  }
-
- 
   return (
     <div className="goods">
       <div className="container">
