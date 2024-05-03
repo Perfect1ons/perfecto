1cf96d3ee1dffd291e44f5d828753b15b2a8493a
Ilya <ilya.yohumming@icloud.com>
2024-05-03 14:48:38 +0600

    complete footer pages

---
+++ src/app/politika/page.tsx
@@ -0,0 +1,23 @@
+import { getFooter, getFooterPages } from "@/api/requests";
+import FooterPageRenderer from "@/components/UI/FooterPageRenderer/FooterPageRenderer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Политика конфиденциальности",
+  description:
+    "Мы рады приветствовать вас и предложить уникальные возможности для карьерного роста в нашей компании.",
+  keywords:
+    "Оптом  Кыргызстан дешево цена розница доставка на заказ интернет магазин Бишкек max.kg характеристики фото",
+  robots: "index,follow",
+};
+export default async function page() {
+  const politika = await getFooterPages("pokupatelyam/politika");
+  const sidebarLinks = await getFooter();
+
+  return (
+    <>
+      <FooterPageRenderer data={politika} links={sidebarLinks} />
+    </>
+  );
+}
