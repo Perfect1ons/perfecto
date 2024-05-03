f103e41b01d7b36484bc4bbc0b5ddc99e850ba0e
Abdulaziz <meisanaziz06@gmail.com>
2024-05-03 16:23:28 +0600

    add catalog

--- src/components/Seek/Seek.tsx
+++ src/components/Seek/Seek.tsx
@@ -1,7 +1,7 @@
 "use client";
-"use client";
-import { ISeekCatalog, ISeekItem, ISeekItems } from "@/types/Search/seek";
+import { ISeekCatalog, ISeekItem } from "@/types/Search/seek";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 const SeekCards = dynamic(() => import("@/components/Seek/SeekCard"));
 
@@ -69,33 +69,56 @@ const Seek: React.FC<ISeekProps> = ({ catalog, product }) => {
   return (
     <section className="seek">
       <div className="container">
-          <div className="sort__buttons">
-            <h2 className="sort__buttons_title">Сортировать:</h2>
-            <button
-              className={`sort__buttons_button ${
-                sortOrder === "cheap" ? "sort__buttons_button_active" : ""
-              }`}
-              onClick={() => handleSort("cheap")}
-            >
-              Сначала дешевле
-            </button>
-            <button
-              className={`sort__buttons_button ${
-                sortOrder === "expensive" ? "sort__buttons_button_active" : ""
-              }`}
-              onClick={() => handleSort("expensive")}
-            >
-              Сначала дороже
-            </button>
-            <button
-              className={`sort__buttons_button ${
-                sortOrder === "rating" ? "sort__buttons_button_active" : ""
-              }`}
-              onClick={() => handleSort("rating")}
-            >
-              По рейтингу
-            </button>
+        <div className="seek__catalog">
+          <h1 className="seek__catalog_title">Найдено в категорях</h1>
+          <div className="seek__catalog_cards">
+            {catalog.map((catalog, index) => {
+              const iconSrc = catalog.icon.startsWith("https://")
+                ? catalog.icon
+                : "https://megabike74.ru/wp-content/themes/chlzuniversal/assets/images/placeholder/placeholder-250x250.jpg";
+              return (
+                <div key={index} className="seek__catalog_card">
+                  <Image
+                    className="seek__catalog_card_image"
+                    src={iconSrc}
+                    width={65}
+                    height={65}
+                    alt={catalog.name}
+                  />
+                  <h1 className="seek__catalog_card_title">{catalog.name}</h1>
+                </div>
+              );
+            })}
           </div>
+        </div>
+
+        <div className="sort__buttons">
+          <h2 className="sort__buttons_title">Сортировать:</h2>
+          <button
+            className={`sort__buttons_button ${
+              sortOrder === "cheap" ? "sort__buttons_button_active" : ""
+            }`}
+            onClick={() => handleSort("cheap")}
+          >
+            Сначала дешевле
+          </button>
+          <button
+            className={`sort__buttons_button ${
+              sortOrder === "expensive" ? "sort__buttons_button_active" : ""
+            }`}
+            onClick={() => handleSort("expensive")}
+          >
+            Сначала дороже
+          </button>
+          <button
+            className={`sort__buttons_button ${
+              sortOrder === "rating" ? "sort__buttons_button_active" : ""
+            }`}
+            onClick={() => handleSort("rating")}
+          >
+            По рейтингу
+          </button>
+        </div>
       </div>
       <div className="main__news_cards">
         {items.map((item, index) => (
---
+++ src/components/Seek/SeekCatalog.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SeekCatalog = () => {
+  return (
+    <div>SeekCatalog</div>
+  )
+}
+
+export default SeekCatalog
