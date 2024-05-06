62f09df76983f89b0df93c67a41d8b9feca4d775
Ilya <ilya.yohumming@icloud.com>
2024-05-06 11:12:03 +0600

    bug fix footer and side bar for footer pages

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -4,43 +4,44 @@ import { IFooterItem } from '@/types/footerRequest';
 import React, { useEffect, useState } from 'react'
 import styles from "./style.module.scss"
 import LinksSidebar from '@/components/UI/LinksSidebar/LinksSidebar';
+import NotFound from '../not-found';
 
 interface IAboutCompanyProps {
     data: IFooterPage | undefined;
     links: IFooterItem[];
-  }
+}
 
-const FooterPage = ({data, links}: IAboutCompanyProps) => {
+const FooterPage = ({ data, links }: IAboutCompanyProps) => {
 
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
-      setIsClient(true);
+        setIsClient(true);
     }, []);
-  
-    if (!data || !data.model.text) {
-      return null;
+
+    if (!data || !data.model || !data.model.text || (data.model.url && data.model.url.startsWith("socseti/"))) {
+        return null;
     }
+
     const html = data.model.text.split("</p>\r\n<p>");
 
-
-  return (
-    <div className={styles.aboutCompany}>
-      <div className={styles.aboutCompanyLinksBar}>
-        <LinksSidebar links={links} />
-      </div>
-      <div className={styles.aboutCompanyInfo}>
-        {isClient &&
-          html.map((paragraph: string, index: number) => (
-            <div
-              className={styles.aboutCompanyItem}
-              key={index}
-              dangerouslySetInnerHTML={{ __html: paragraph }}
-            />
-          ))}
-      </div>
-    </div>
-  )
+    return (
+        <div className={styles.wrap}>
+            <div className={styles.wrapSidebar}>
+                <LinksSidebar links={links} />
+            </div>
+            <div className={styles.wrapInfo}>
+                {isClient &&  
+                    html.map((paragraph: string, index: number) => (
+                        <div
+                            className={styles.wrapInfoItem}
+                            key={index}
+                            dangerouslySetInnerHTML={{ __html: paragraph }}
+                        />
+                    ))}
+            </div>
+        </div>
+    )
 }
 
 export default FooterPage
