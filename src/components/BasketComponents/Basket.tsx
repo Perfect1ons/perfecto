5e1c731c3630bafaee592ed309d9b3d0f3281e3c
Эндарбек <endarashiraliev@gmail.com>
2024-05-28 17:02:08 +0600

    add product in cart basket

--- src/components/BasketComponents/Basket.tsx
+++ src/components/BasketComponents/Basket.tsx
@@ -1,12 +1,25 @@
 "use client";
+import cn from "clsx";
 import styles from "./style.module.scss";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { Items } from "@/types/CardProduct/cardProduct";
+import {
+  CartIcon,
+  GrayFavoritesIcon,
+  GrayStar,
+  VioletFavoritesIcon,
+  YellowStar,
+} from "../../../public/Icons/Icons";
+import { useState } from "react";
+import { url } from "../temporary/data";
 
 const Basket = () => {
+  const [rating, setRating] = useState(0);
+  const [isFavorite, setIsFavorite] = useState(false);
   const data = useSelector((store: RootState) => store.cart);
+
   return (
     <div className={styles.container}>
       {data.cart.length === 0 ? (
@@ -25,16 +38,92 @@ const Basket = () => {
         </div>
       ) : (
         <div>
-          <h2>Содержимое корзины:</h2>
-          <ul>
-            {data.cart.map((product: Items) => (
-              <li key={product.id}>
-                <div>{product.name}</div>
-                <div>{product.price}</div>
-                {/* Добавьте другие свойства продукта, которые вы хотите отобразить */}
-              </li>
-            ))}
-          </ul>
+          <div>
+            <h1>Корзина</h1>
+          </div>
+          {data.cart.map((item) => {
+            return (
+              <div key={item.id_tov} className="default__card_column">
+                <div className="default__card_column_right">
+                  <div className="default__card_images_column">
+                    <Image
+                      className="default__card_image_column"
+                      src={`/img/undefinedPage.png`}
+                      width={200}
+                      height={200}
+                      alt={item.naim}
+                      quality={100}
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="default__card_info_column">
+                    <h2 className="default__card_name_column">{item.naim}</h2>
+                    <p className="card__column_id">Код: {item.id_tov}</p>
+                    <div className="ocenka_column">
+                      {[...Array(5)].map((_, index) => (
+                        <span key={index}>
+                          {index < rating ? <YellowStar /> : <GrayStar />}
+                        </span>
+                      ))}
+                    </div>
+                    <div className="ddos__column">
+                      <Image
+                        src={`${url}images/delivery_icon.svg`}
+                        width={20}
+                        height={20}
+                        alt="delivery_icon"
+                      />
+                      <p className="ddos__text_column">{item.ddos}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="default__card_buttons_column">
+                  <div className="default__card_buttons_column_price">
+                    <span className="default__card_price">
+                      {item.cenaok.toLocaleString("ru-RU")}
+                      <span className="default__card_price_custom"> с</span>
+                    </span>
+                    <button
+                      title="Добавить в избранное"
+                      className={cn("add__to_fav", {
+                        ["add__to_fav_active"]: isFavorite,
+                      })}
+                    >
+                      <span className="add__to_fav_icon">
+                        {isFavorite ? (
+                          <VioletFavoritesIcon />
+                        ) : (
+                          <GrayFavoritesIcon />
+                        )}
+                      </span>
+                    </button>
+                  </div>
+
+                  {item.minQty > 1 ? (
+                    <h3 className="minimal__items">
+                      минимальное количество к заказу от {item.minQty} шт.
+                    </h3>
+                  ) : null}
+
+                  <div className="add__to_cart_column">
+                    <button
+                      title="Добавить в корзину"
+                      className="add__to_cart_column_button"
+                      onClick={() => console.log("Добавлено в корзину")}
+                    >
+                      <span className="add__to_cart_icon">
+                        <CartIcon />
+                      </span>
+                      В корзину
+                    </button>
+                    <button className="add__to_cart_column_button column_buy">
+                      Купить
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
