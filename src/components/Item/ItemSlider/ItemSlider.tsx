bb272207727d392d1bf8eb4300f91e6fc030d161
Yehikin <nurbolotnurdin@gmail.com>
2024-05-31 14:41:20 +0600

    add empty image to the item image

--- src/components/Item/ItemSlider/ItemSlider.tsx
+++ src/components/Item/ItemSlider/ItemSlider.tsx
@@ -54,51 +54,57 @@ const ItemSlider = ({ photos }: IPhotosProps) => {
 
   return (
     <div className={styles.product__swipers}>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        slidesPerView={6}
-        direction={"vertical"}
-        spaceBetween={10}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className={clsx(styles.product__cards, "mySwiper")}
-      >
-        {photos.video && (
-          <SwiperSlide
-            className={styles.product__cards_item}
-            onMouseEnter={() => handleMouseEnter(0)}
+      {photos.photos.length > 0 ? (
+        <>
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            slidesPerView={6}
+            direction={"vertical"}
+            spaceBetween={10}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className={clsx(styles.product__cards, "mySwiper")}
           >
-            <div className={clsx(styles.product_preview, "thumb-actived")}>
-              <SmallVideoPreview />
-            </div>
-          </SwiperSlide>
-        )}
-        {photos.photos.map((photo, index) => (
-          <SwiperSlide
-            className={styles.product__cards_item}
-            key={index}
-            onMouseEnter={() =>
-              handleMouseEnter(photos.video ? index + 1 : index)
-            }
-          >
-            <Image
-              className={clsx(styles.product_preview, "thumb-actived")}
-              src={
-                photo.url_part.startsWith("https://goods")
-                  ? `${photo.url_part}280.jpg`
-                  : photo.url_part.startsWith("https://")
-                  ? photo.url_part
-                  : `${url}nal/img/${photos.id_post}/l_${photo.url_part}`
-              }
-              width={100}
-              height={100}
-              alt={photo.url_part}
-              loading="lazy"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            {photos.video && (
+              <SwiperSlide
+                className={styles.product__cards_item}
+                onMouseEnter={() => handleMouseEnter(0)}
+              >
+                <div className={clsx(styles.product_preview, "thumb-actived")}>
+                  <SmallVideoPreview />
+                </div>
+              </SwiperSlide>
+            )}
+            {photos.photos.map((photo, index) => (
+              <SwiperSlide
+                className={styles.product__cards_item}
+                key={index}
+                onMouseEnter={() =>
+                  handleMouseEnter(photos.video ? index + 1 : index)
+                }
+              >
+                <Image
+                  className={clsx(styles.product_preview, "thumb-actived")}
+                  src={
+                    photo.url_part.startsWith("https://goods")
+                      ? `${photo.url_part}280.jpg`
+                      : photo.url_part.startsWith("https://")
+                      ? photo.url_part
+                      : `${url}nal/img/${photos.id_post}/l_${photo.url_part}`
+                  }
+                  width={100}
+                  height={100}
+                  alt={photo.url_part}
+                  loading="lazy"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      ) : (
+        " "
+      )}
       <Swiper
         onSwiper={setMainSwiper}
         keyboard={{
@@ -126,34 +132,47 @@ const ItemSlider = ({ photos }: IPhotosProps) => {
             ></div>
           </SwiperSlide>
         )}
-        {photos.photos.slice(0, 7).map((photo, index) => (
-          <SwiperSlide
-            key={photos.video ? index + 1 : index}
-            className={styles.activeSlide}
-          >
-            <InnerImageZoom
+        {photos.photos.length > 0 ? (
+          photos.photos.slice(0, 7).map((photo, index) => (
+            <SwiperSlide
+              key={photos.video ? index + 1 : index}
+              className={styles.activeSlide}
+            >
+              <InnerImageZoom
+                width={500}
+                height={500}
+                src={
+                  photo.url_part.startsWith("https://goods")
+                    ? `${photo.url_part}280.jpg`
+                    : photo.url_part.startsWith("https://")
+                    ? photo.url_part
+                    : `${url}nal/img/${photos.id_post}/b_${photo.url_part}`
+                }
+                zoomSrc={
+                  photo.url_part.startsWith("https://goods")
+                    ? `${photo.url_part}280.jpg`
+                    : photo.url_part.startsWith("https://")
+                    ? photo.url_part
+                    : `${url}nal/img/${photos.id_post}/b_${photo.url_part}`
+                }
+                zoomType="hover"
+                zoomScale={1.7}
+                className={styles.product_img}
+              />
+            </SwiperSlide>
+          ))
+        ) : (
+          <SwiperSlide className={styles.activeSlide}>
+            <Image
+              className={styles.product_img}
+              src="/img/blurImage.png"
               width={500}
               height={500}
-              src={
-                photo.url_part.startsWith("https://goods")
-                  ? `${photo.url_part}280.jpg`
-                  : photo.url_part.startsWith("https://")
-                  ? photo.url_part
-                  : `${url}nal/img/${photos.id_post}/b_${photo.url_part}`
-              }
-              zoomSrc={
-                photo.url_part.startsWith("https://goods")
-                  ? `${photo.url_part}280.jpg`
-                  : photo.url_part.startsWith("https://")
-                  ? photo.url_part
-                  : `${url}nal/img/${photos.id_post}/b_${photo.url_part}`
-              }
-              zoomType="hover"
-              zoomScale={1.7}
-              className={styles.product_img}
+              alt="empty.png"
+              loading="lazy"
             />
           </SwiperSlide>
-        ))}
+        )}
         <button
           className={clsx(
             styles.sliderArrow,
