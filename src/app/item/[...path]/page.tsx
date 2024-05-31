c11077017d70e9bcd6900e58cef964dd7ff2fdbe
Эндарбек <endarashiraliev@gmail.com>
2024-05-31 17:45:34 +0600

    fix

--- src/app/item/[...path]/page.tsx
+++ src/app/item/[...path]/page.tsx
@@ -9,6 +9,23 @@ interface Params {
   params: { path: string };
 }
 
+export default async function item({ params: { path } }: Params) {
+  const data = await getCardProduct(path[0]);
+
+  const [breadCrumbs, similarData] = await Promise.all([
+    getBreadCrumbs(data.items.id_cat),
+    getSimilarProduct(path[0]),
+  ]);
+
+  return (
+    <ItemPage
+      data={data.items}
+      similar={similarData}
+      breadCrumbs={breadCrumbs}
+    />
+  );
+}
+
 export async function generateMetadata({ params: { path } }: Params) {
   const data = await getCardProduct(path[0]);
 
@@ -37,21 +54,4 @@ export async function generateMetadata({ params: { path } }: Params) {
       type: "article",
     },
   };
-}
-
-export default async function item({ params: { path } }: Params) {
-  const data = await getCardProduct(path[0]);
-
-  const [breadCrumbs, similarData] = await Promise.all([
-    getBreadCrumbs(data.items.id_cat),
-    getSimilarProduct(path[0]),
-  ]);
-
-  return (
-    <ItemPage
-      data={data.items}
-      similar={similarData}
-      breadCrumbs={breadCrumbs}
-    />
-  );
 }
