9c99bbad7ff5f6f0bfa02fe539a51d0d18004ed6
Эндарбек <endarashiraliev@gmail.com>
2024-06-03 16:50:02 +0600

    fix

--- src/components/Item/ItemBanner/ItemBanner.tsx
+++ src/components/Item/ItemBanner/ItemBanner.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 const ItemBanner = () => {
   return (
     <Image
+      className="testoviy"
       src="https://avatars.mds.yandex.net/get-market-adv/8282799/a128394f-da2d-4fdb-aa92-083f5cd7e273/1080x450"
       width={344}
       height={143}
