c0e6effdc8a88e26598664b381db527b9612edde
Ilya <ilya.yohumming@icloud.com>
2024-07-03 14:20:27 +0600

    skeleton jump fix

--- src/components/Item/ItemMainSkeleton/ItemMainSkeleton.tsx
+++ src/components/Item/ItemMainSkeleton/ItemMainSkeleton.tsx
@@ -4,11 +4,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import styles from "./style.module.scss";
 import { ICardProductItems } from "@/types/CardProduct/cardProduct";
 
-interface ItemMainSkeletonProps {
-  data: ICardProductItems;
-}
-
-export default function ItemMainSkeleton({ data }: ItemMainSkeletonProps) {
+export default function ItemMainSkeleton() {
   const skeletonArray6 = new Array(6).fill(null);
   const skeletonArray3 = new Array(3).fill(null);
 
@@ -22,15 +18,13 @@ export default function ItemMainSkeleton({ data }: ItemMainSkeletonProps) {
 
       <div className={styles.content_wrap}>
         <div className={styles.sliders_wrap}>
-          {data.items.photos.length > 0 && (
-            <div className={styles.mini_slider}>
-              {skeletonArray6.map((_, index) => (
-                <span key={index}>
-                  <Skeleton width={60} height={60} />
-                </span>
-              ))}
-            </div>
-          )}
+          <div className={styles.mini_slider}>
+            {skeletonArray6.map((_, index) => (
+              <span key={index}>
+                <Skeleton width={60} height={60} />
+              </span>
+            ))}
+          </div>
           <div className={styles.main_slider}>
             <Skeleton width={500} height={500} />
           </div>
@@ -43,19 +37,15 @@ export default function ItemMainSkeleton({ data }: ItemMainSkeletonProps) {
           </div>
           <div className={styles.info}>
             <div className={styles.info_part}>
-              {data.items.description && (
-                <div className={styles.desc}>
-                  <Skeleton width={90} height={20} />
-                  <Skeleton width={400} height={160} />
-                </div>
-              )}
+              <div className={styles.desc}>
+                <Skeleton width={90} height={20} />
+                <Skeleton width={400} height={160} />
+              </div>
               <Skeleton width={400} height={20} className={styles.copyArt} />
-              {data.items.specification && (
-                <div className={styles.chars}>
-                  <Skeleton width={130} height={20} />
-                  <Skeleton width={400} height={160} />
-                </div>
-              )}
+              <div className={styles.chars}>
+                <Skeleton width={130} height={20} />
+                <Skeleton width={400} height={160} />
+              </div>
             </div>
 
             <div className={styles.price_part}>
