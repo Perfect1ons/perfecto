bf9104302bc1bd0ac7ae7a58189372542b78df2f
Abdulaziz <meisanaziz06@gmail.com>
2024-07-09 17:48:34 +0600

    fixe

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,6 @@
-export function truncateText(text: string, maxLength: number): string {
+export function truncateText(text: any, maxLength: number): string {
+  if (typeof text !== "string") {
+    return "";
+  }
   return text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
 }
