a3eac6209f7aee6031157c70753a4a5b364f29f5
Abdulaziz <meisanaziz06@gmail.com>
2024-07-09 16:03:46 +0600

    fix

--- src/components/Seek/ProductList.tsx
+++ src/components/Seek/ProductList.tsx
@@ -1,25 +1,19 @@
 import { ISeekItem } from "@/types/Search/seek";
 import dynamic from "next/dynamic";
-import SeekCardColumn from "./SeekCardColumn";
 
 const SeekCards = dynamic(() => import("@/components/Seek/SeekCard"));
 
 interface ProductListProps {
   items: ISeekItem[];
-  isColumnView: boolean; // Добавляем пропс для определения вида отображения
 }
 
-const ProductList: React.FC<ProductListProps> = ({ items, isColumnView }) => {
+const ProductList: React.FC<ProductListProps> = ({ items }) => {
   return (
     <div
-      className={isColumnView ? "main__news_cards_column" : "main__news_cards"}
+      className={"main__news_cards"}
     >
       {items.map((item, index) =>
-        isColumnView ? (
-          <SeekCardColumn key={index} cardData={item} />
-        ) : (
-          <SeekCards key={index} cardData={item} />
-        )
+        <SeekCards key={index} cardData={item} />
       )}
     </div>
   );
--- src/components/Seek/Seek.tsx
+++ src/components/Seek/Seek.tsx
@@ -1,8 +1,6 @@
 "use client";
-import React, { useState, useEffect } from "react";
 import { ISeekCatalog, ISeekItem } from "@/types/Search/seek";
 import ProductList from "./ProductList";
-import CustomSelect from "./CustomSelect";
 import SeekCatalog from "./SeekCatalog";
 interface SeekProps {
   catalog: ISeekCatalog[];
@@ -10,51 +8,13 @@ interface SeekProps {
 }
 
 const Seek: React.FC<SeekProps> = ({ catalog, product }) => {
-  const [items, setItems] = useState<ISeekItem[]>(product);
-  const [isColumnView, setIsColumnView] = useState(false);
-
-  const handleViewChange = (isColumn: boolean) => {
-    setIsColumnView(isColumn);
-  };
-
   return (
     <section className="seek">
       <div className="container">
-          <h1 className="seek__catalog_title">Найдено в категориях</h1>
-          <SeekCatalog catalog={catalog}/>
-        <div className="sort__buttons">
-          <div className="default__sort_style">
-            <button
-              className="default__sort_icons"
-              onClick={() => handleViewChange(false)}
-            >
-              {[...Array(4)].map((_, index) => (
-                <div
-                  key={index}
-                  className={`default__sort_icon ${
-                    !isColumnView ? "sort__button_icons_active" : ""
-                  }`}
-                ></div>
-              ))}
-            </button>
-
-            <button
-              className="default__sort_icons_column"
-              onClick={() => handleViewChange(true)}
-            >
-              {[...Array(2)].map((_, index) => (
-                <div
-                  key={index}
-                  className={`default__sort_icon_column ${
-                    isColumnView ? "sort__button_icons_active" : ""
-                  }`}
-                ></div>
-              ))}
-            </button>
-          </div>
-        </div>
+        <h1 className="seek__catalog_title">Найдено в категориях</h1>
+        <SeekCatalog catalog={catalog}/>
       </div>
-      <ProductList items={product} isColumnView={isColumnView} />
+      <ProductList items={product}  />
     </section>
   );
 };
