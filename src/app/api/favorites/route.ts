d545ba8a86d1470d806a6a8ad651a1f2f888cf5a
Ilya <ilya.yohumming@icloud.com>
2024-07-10 14:33:49 +0600

    add route for favorites

---
+++ src/app/api/favorites/route.ts
@@ -0,0 +1,133 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+interface FavoriteData {
+  id: string;
+  id_tov: string;
+  id_post: string;
+  old_price: number;
+  discount_prc: number;
+  naim: string;
+  ddos: string;
+  cenaok: number;
+  url: string;
+  photos: string[];
+  ocenka: number;
+  status: string;
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const favoriteData: FavoriteData = await req.json(); // Получение данных из тела запроса
+
+    const cookieStore = cookies(); // Получение кук с помощью next/headers
+    const existingFavorites = JSON.parse(
+      cookieStore.get("favoriteItems")?.value || "[]"
+    );
+
+    // Проверяем, есть ли уже такой товар в избранном
+    const isAlreadyFavorite = existingFavorites.some(
+      (item: FavoriteData) => item.id === favoriteData.id
+    );
+
+    if (isAlreadyFavorite) {
+      return NextResponse.json({
+        success: true,
+        message: "Item already in favorites",
+      });
+    }
+
+    // Добавляем новый товар в список избранных
+    const updatedFavorites = [...existingFavorites, favoriteData];
+
+    cookieStore.set("favoriteItems", JSON.stringify(updatedFavorites), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 60 * 60 * 24 * 90, // 3 месяца
+      sameSite: "strict",
+      path: "/",
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { success: false, error: error.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { success: false, error: "Unknown error" },
+        { status: 500 }
+      );
+    }
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { id }: { id: string } = await req.json(); // Получение данных из тела запроса
+
+    const cookieStore = cookies(); // Получение кук с помощью next/headers
+    const existingFavorites = JSON.parse(
+      cookieStore.get("favoriteItems")?.value || "[]"
+    );
+
+    // Фильтруем избранные товары, исключая указанный id
+    const updatedFavorites = existingFavorites.filter(
+      (item: FavoriteData) => item.id !== id
+    );
+
+    cookieStore.set("favoriteItems", JSON.stringify(updatedFavorites), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 60 * 60 * 24 * 90, // 3 месяца
+      sameSite: "strict",
+      path: "/",
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { success: false, error: error.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { success: false, error: "Unknown error" },
+        { status: 500 }
+      );
+    }
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const { favoriteItems }: { favoriteItems: FavoriteData[] } =
+      await req.json(); // Получение данных из тела запроса
+
+    const cookieStore = cookies(); // Получение кук с помощью next/headers
+    cookieStore.set("favoriteItems", JSON.stringify(favoriteItems), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 60 * 60 * 24 * 90, // 3 месяца
+      sameSite: "strict",
+      path: "/",
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { success: false, error: error.message },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { success: false, error: "Unknown error" },
+        { status: 500 }
+      );
+    }
+  }
+}
