9b87cda5261aacb6f70e4a673121b2982615f43a
Ilya <ilya.yohumming@icloud.com>
2024-07-12 16:16:31 +0600

    add pagination cart

--- src/components/BasketComponents/BasketProducts/BasketProducts.tsx
+++ src/components/BasketComponents/BasketProducts/BasketProducts.tsx
@@ -11,6 +11,8 @@ import { url } from "@/components/temporary/data";
 import { ICard } from "@/types/Card/card";
 import FavoriteModal from "@/components/FavoritesComponents/FavoritesModal/FavoritesModal";
 import BasketCard from "./BasketCard/BasketCard";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ReactPaginate from "react-paginate";
 const BasketProducts = () => {
   const [isModalVisible, setModalVisible] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
@@ -41,6 +43,15 @@ const BasketProducts = () => {
   const [added, setAdded] = useState(false);
   const [shouldFocusInput, setShouldFocusInput] = useState(false);
   const [allItemsSelected, setAllItemsSelected] = useState(false); // State to track if all items are selecte
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 20; // Показывать по 20 товаров на странице
+
+  const isMobile = useMediaQuery("(max-width: 480px)");
+
+  const offset = currentPage * itemsPerPage;
+  const currentItems = data.cart.slice(offset, offset + itemsPerPage);
+
   const handleToggleAllItems = () => {
     setAllItemsSelected(!allItemsSelected);
   };
@@ -111,6 +122,20 @@ const BasketProducts = () => {
     dispatch(toggleProductSelection(id));
   };
 
+  const handlePageClick = ({ selected }: { selected: number }) => {
+    setCurrentPage(selected);
+    const newPage = selected + 1;
+    const queryParams = new URLSearchParams();
+
+    if (newPage > 0) queryParams.set("page", newPage.toString());
+    const newUrl = `${window.location.pathname}?${queryParams.toString()}`;
+    window.history.replaceState({ path: newUrl }, "", newUrl);
+    window.scrollTo({ top: 0, behavior: "auto" });
+  };
+
+  // Calculate total pages based on favorites length and itemsPerPage
+  const pageCount = Math.ceil(data.cart.length / itemsPerPage);
+
   return (
     <div className={styles.cardsAllContainer}>
       <FavoriteModal
@@ -119,7 +144,7 @@ const BasketProducts = () => {
         isRedirect={isRedirect}
         onClose={handleModalClose}
       />
-      {data.cart.map((item) => {
+      {currentItems.map((item) => {
         const imageUrl =
           item.photos.length > 0
             ? item.photos[0]?.url_part.startsWith("https://goods")
@@ -145,6 +170,28 @@ const BasketProducts = () => {
           />
         );
       })}
+      {pageCount > 1 && (
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          forcePage={currentPage}
+          breakLabel={isMobile ? ".." : "..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={isMobile ? 2 : 3}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          pageClassName={"page-item"}
+          pageLinkClassName={"page-link"}
+          previousClassName={"page-item-btn"}
+          previousLinkClassName={"page-link-previous"}
+          nextClassName={"page-item-btn"}
+          nextLinkClassName={"page-link-next"}
+          breakClassName={"page-item"}
+          breakLinkClassName={"page-link"}
+          activeClassName={"active"}
+        />
+      )}
     </div>
   );
 };
