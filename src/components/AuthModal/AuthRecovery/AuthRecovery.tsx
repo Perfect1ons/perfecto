4767ab53961f42969ad80d3b0417e8dcfe8fabcf
Эндарбек <endarashiraliev@gmail.com>
2024-07-15 16:31:47 +0600

    fix

--- src/components/AuthModal/AuthRecovery/AuthRecovery.tsx
+++ src/components/AuthModal/AuthRecovery/AuthRecovery.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ChangeEvent, useCallback, useMemo, useState } from "react";
+import React, { ChangeEvent } from "react";
 import cn from "clsx";
 import styles from "./style.module.scss";
 import InputMask from "react-input-mask";
@@ -9,6 +9,13 @@ import { ArrowDropdown } from "../../../../public/Icons/Icons";
 interface FormProps {
   setView: (view: "login" | "recovery" | "registration" | "confirm") => void;
   close: () => void;
+  handleBuyerChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  countryOptions: any;
+  mask: any;
+  currentCodeCountry: Country;
+  visible: string;
+  visibleHandler: (current: string) => void;
+  phoneNumber: string;
 }
 
 interface Country {
@@ -17,77 +24,21 @@ interface Country {
   name: string;
 }
 
-const codesCountry: Record<string, Country> = {
-  kg: { code: 996, img: "/img/kgFlag.svg", name: "Кыргызстан" },
-  ru: { code: 7, img: "/img/ruFlag.svg", name: "Россия" },
-  kz: { code: 7, img: "/img/kzFlag.svg", name: "Казахстан" },
-};
-
-type CountryKey = keyof typeof codesCountry;
-
-const AuthRecovery = ({ setView, close }: FormProps) => {
-  const [visible, setVisible] = useState<string>("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-
-  const [currentCodeCountry, setCurrentCodeCountry] = useState<Country>(
-    codesCountry.kg
-  );
-
-  const getMaskForCountry = (code: number) => {
-    switch (code) {
-      case 996:
-        return "\\+\\9\\96 (999) 99-99-99";
-      case 7:
-        return "\\+\\7 (999) 999-99-99";
-      default:
-        return "\\+\\9\\96 (999) 99-99-99";
-    }
-  };
-
-  const [mask, setMask] = useState(getMaskForCountry(currentCodeCountry.code));
-
-  const visibleHandler = useCallback((current: string) => {
-    setVisible((prevVisible) => (prevVisible !== current ? current : ""));
-  }, []);
-
-  const handleBuyerChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setPhoneNumber(value);
-  }, []);
-
+const AuthRecovery = ({
+  setView,
+  close,
+  countryOptions,
+  currentCodeCountry,
+  handleBuyerChange,
+  mask,
+  visible,
+  visibleHandler,
+  phoneNumber,
+}: FormProps) => {
   const handleSubmit = (event: any) => {
     event.preventDefault();
   };
 
-  const codeCountryHandler = useCallback((country: CountryKey) => {
-    const selectedCountry = codesCountry[country];
-    setCurrentCodeCountry(selectedCountry);
-    setPhoneNumber(`+${selectedCountry.code}`);
-    setMask(getMaskForCountry(selectedCountry.code));
-    setVisible("");
-  }, []);
-
-  const countryOptions = useMemo(() => {
-    return Object.entries(codesCountry).map(([key, country]) => (
-      <button
-        key={key}
-        onClick={() => codeCountryHandler(key as CountryKey)}
-        className={styles.modal__form_phone_dropdown_button}
-      >
-        <Image
-          className={styles.modal__form_phone_dropdown_button_img}
-          src={country.img}
-          width={30}
-          height={30}
-          alt={country.name}
-        />
-        {country.name}
-        <span className={styles.modal__form_phone_dropdown_button_code}>
-          +{country.code}
-        </span>
-      </button>
-    ));
-  }, [codeCountryHandler]);
   return (
     <>
       <p className={styles.modal__text}>
