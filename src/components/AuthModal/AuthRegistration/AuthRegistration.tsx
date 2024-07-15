3e4fa7d648af42ea776c6dcede3dffc8c0a102ac
Ilya <ilya.yohumming@icloud.com>
2024-07-15 15:13:58 +0600

    add post auth

--- src/components/AuthModal/AuthRegistration/AuthRegistration.tsx
+++ src/components/AuthModal/AuthRegistration/AuthRegistration.tsx
@@ -5,6 +5,7 @@ import { ArrowDropdown } from "../../../../public/Icons/Icons";
 import Image from "next/image";
 import InputMask from "react-input-mask";
 import { ChangeEvent, useCallback, useMemo, useState } from "react";
+import { postLoginCode } from "@/api/clientRequest";
 interface FormProps {
   setView: (view: "login" | "recovery" | "registration") => void;
   close: () => void;
@@ -56,6 +57,27 @@ const AuthRegistration = ({ setView, close }: FormProps) => {
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
+    const cleanedPhoneNumber = phoneNumber.replace(/\D/g, "");
+
+    let expectedLength = 0;
+    switch (currentCodeCountry.code) {
+      case 996:
+        expectedLength = 12;
+        break;
+      case 7:
+        expectedLength = 11;
+        break;
+      default:
+        expectedLength = 12;
+        break;
+    }
+
+    if (cleanedPhoneNumber.length !== expectedLength) {
+      console.log("Phone number length is incorrect for the selected country.");
+      return;
+    }
+
+    postLoginCode(cleanedPhoneNumber);
   };
 
   const codeCountryHandler = useCallback((country: CountryKey) => {
@@ -141,6 +163,7 @@ const AuthRegistration = ({ setView, close }: FormProps) => {
           )}
         </div>
         <button
+          onClick={handleSubmit}
           className={cn(styles.modal__button, "button")}
           aria-label="go to registration"
           type="submit"
