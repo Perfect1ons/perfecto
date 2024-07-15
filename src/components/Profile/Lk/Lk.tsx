faa3b78d7a3099271f5bcebf7a775a5c90086aa9
Abdulaziz <meisanaziz06@gmail.com>
2024-07-15 17:47:34 +0600

    fix

--- src/components/Profile/Lk/Lk.tsx
+++ src/components/Profile/Lk/Lk.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import styles from './style.module.scss'
 
 const Lk = () => {
   return (
-    <div>
-        <h1>ЛИЧНЫЙ КАБИНЕТ</h1>
-    </div>
-  )
+    <section className={styles.lk}>
+      <div className="container">
+        <div>
+          <div>
+            <form className={styles.lk__form}>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+              <input className={styles.lk__input} type="text" placeholder='value'/>
+            </form>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Lk
