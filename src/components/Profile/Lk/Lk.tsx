b938f174940d34742e0eae8fa725755ab97767d0
Эндарбек <endarashiraliev@gmail.com>
2024-07-16 11:56:29 +0600

    fic

--- src/components/Profile/Lk/Lk.tsx
+++ src/components/Profile/Lk/Lk.tsx
@@ -1,30 +1,19 @@
-import styles from './style.module.scss'
+import UserPersonalData from "./UserPersonalData/UserPersonalData";
+import styles from "./style.module.scss";
 
 const Lk = () => {
   return (
     <section className={styles.lk}>
       <div className="container">
+        <UserPersonalData />
         <div>
-          <div>
-            <form className={styles.lk__form}>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-              <input className={styles.lk__input} type="text" placeholder='value'/>
-            </form>
-          </div>
+          <div></div>
 
           <div></div>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Lk
+export default Lk;
