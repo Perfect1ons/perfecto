b392f282dc9d6f66480a8f41347720205520e41a
Эндарбек <endarashiraliev@gmail.com>
2024-07-16 12:59:24 +0600

    fix

--- src/components/Profile/Lk/UserPersonalData/UserPersonalData.tsx
+++ src/components/Profile/Lk/UserPersonalData/UserPersonalData.tsx
@@ -1,22 +1,75 @@
+"use client";
+import { useState } from "react";
 import styles from "./style.module.scss";
-
+import Image from "next/image";
+import cn from "clsx";
 const UserPersonalData = () => {
+  const [formData, setFormData] = useState({
+    lastName: "",
+    firstName: "",
+    birthDate: "",
+    city: "",
+    deliveryAddress: "",
+    mobileNUmber: "",
+    workPlace: "",
+    position: "",
+    organizationName: "",
+    comments: "",
+    isOver: false,
+  });
+  const handleChange = (event: any) => {
+    setFormData((formData) => {
+      return {
+        ...formData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+  const handleCheckboxChange = () => {
+    setFormData((prevState) => ({
+      ...prevState,
+      isOver: !prevState.isOver,
+    }));
+  };
   return (
     <div className={styles.containerPersonalData}>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          required
+        />
         <label className={styles.inputLabel}>Фамилия</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          name="firstName"
+          onChange={handleChange}
+          type="text"
+          required
+        />
         <label className={styles.inputLabel}>Имя</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          type="text"
+          required
+          name="birthDate"
+          onChange={handleChange}
+        />
         <label className={styles.inputLabel}>Дата рождения</label>
       </div>
       <div className={styles.inputContainer}>
-        <select defaultValue="" className={styles.cityContainer}>
+        <select
+          defaultValue=""
+          className={styles.cityContainer}
+          name="city"
+          onChange={handleChange}
+        >
           <option value="1">Бишкек</option>
           <option value="2">Ош</option>
           <option value="3">Чуйская область</option>
@@ -30,31 +83,87 @@ const UserPersonalData = () => {
         </select>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          name="deliveryAddress"
+          onChange={handleChange}
+          type="text"
+          required
+        />
         <label className={styles.inputLabel}>Адрес доставки</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          type="text"
+          required
+          name="mobileNUmber"
+          onChange={handleChange}
+        />
         <label className={styles.inputLabel}>Мобильный номер</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          name="workPlace"
+          onChange={handleChange}
+          type="text"
+          required
+        />
         <label className={styles.inputLabel}>Место работы</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          type="text"
+          required
+          name="position"
+          onChange={handleChange}
+        />
         <label className={styles.inputLabel}>Должность</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          type="text"
+          required
+          name="organizationName"
+          onChange={handleChange}
+        />
         <label className={styles.inputLabel}>Название организации, ИНН</label>
       </div>
       <div className={styles.inputContainer}>
-        <input className={styles.inputField} type="text" required />
+        <input
+          className={styles.inputField}
+          type="text"
+          required
+          name="comments"
+          onChange={handleChange}
+        />
         <label className={styles.inputLabel}>Коментарии</label>
       </div>
-      <div className={styles.checkBoxContainer}>
-        <input type="checkbox" className={styles.checkBoxContainer__check} />
+      <div className={styles.checkBoxContainer} onClick={handleCheckboxChange}>
+        <span
+          className={cn("showFiltersUlContainer__check", {
+            ["showFiltersUlContainer__checkActive"]: formData.isOver,
+          })}
+        >
+          {formData.isOver ? (
+            <Image
+              src="/img/checkIconWhite.svg"
+              width={15}
+              height={15}
+              alt="check"
+            />
+          ) : (
+            <Image
+              src="/img/checkIconWhite.svg"
+              width={15}
+              height={15}
+              alt="check"
+            />
+          )}
+        </span>
         <label>Мне больше 18</label>
       </div>
       <p>* - поля, обязательные для заполнения.</p>
