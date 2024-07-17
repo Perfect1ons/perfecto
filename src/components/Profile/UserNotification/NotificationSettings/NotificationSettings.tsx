3fa14a0f7db8b26e6a39912b7e9a8d2c8bfbb280
Abdulaziz <meisanaziz06@gmail.com>
2024-07-17 10:44:16 +0600

    add types

---
+++ src/components/Profile/UserNotification/NotificationSettings/NotificationSettings.tsx
@@ -0,0 +1,15 @@
+import styles from './style.module.scss'
+
+const NotificationSettings = () => {
+  return (
+    <section className={styles.settings}>
+      <div className="container">
+        <div className={styles.settings__container}>
+            <h1>hello world</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default NotificationSettings
