f5356034f2c430c4e66cc0191ff3553ae9b4608d
Эндарбек <endarashiraliev@gmail.com>
2024-07-22 14:10:18 +0600

    фиксики

--- src/components/AuthModal/AuthConfirmCode/AuthConfirmCode.tsx
+++ src/components/AuthModal/AuthConfirmCode/AuthConfirmCode.tsx
@@ -24,17 +24,19 @@ const AuthConfirmCode = ({
   const [code, setCode] = useState(["", "", "", ""]);
   const [warning, setWarning] = useState("");
   const [attemptCount, setAttemptCount] = useState(0);
-  const [lodaing, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const [invalidCodeMessage, setInvalidCodeMessage] = useState("");
   const [timer, setTimer] = useState(0); // Добавляем состояние для таймера
   const [canResend, setCanResend] = useState(false); // Флаг для кнопки повторной отправки
+  const lastFocusedIndex = useRef<number | null>(null);
   const inputRefs = useRef<Array<HTMLInputElement | null>>([
     null,
     null,
     null,
     null,
   ]);
+
   useEffect(() => {
     if (isButtonDisabled) {
       setCanResend(false);
@@ -94,6 +96,7 @@ const AuthConfirmCode = ({
     if (value && index < 3) {
       // Если введена цифра и это не последний инпут, переводим фокус на следующий инпут
       inputRefs.current[index + 1]?.focus();
+      lastFocusedIndex.current = index + 1;
     }
   };
 
@@ -113,6 +116,7 @@ const AuthConfirmCode = ({
         newCode[index - 1] = "";
         setCode(newCode);
         inputRefs.current[index - 1]?.focus();
+        lastFocusedIndex.current = index - 1;
       }
     } else if (!/^\d$/.test(event.key)) {
       // Если нажатая клавиша не цифра, предотвращаем ввод
@@ -192,6 +196,11 @@ const AuthConfirmCode = ({
       handleSubmit();
     }
   }, [code]);
+  useEffect(() => {
+    if (!loading && lastFocusedIndex.current !== null) {
+      inputRefs.current[lastFocusedIndex.current]?.focus();
+    }
+  }, [loading]);
   if (invalidCodeMessage) {
     setTimeout(() => {
       setInvalidCodeMessage("");
@@ -200,7 +209,7 @@ const AuthConfirmCode = ({
 
   return (
     <>
-      {lodaing ? (
+      {loading ? (
         <div className={styles.loader__container}>
           <div className={styles.loader}></div>
         </div>
@@ -217,13 +226,16 @@ const AuthConfirmCode = ({
                 maxLength={1}
                 onChange={(e) => handleChange(index, e.target.value)}
                 onKeyDown={(e) => handleKeyDown(index, e)}
-                autoFocus={index === 0}
                 ref={(el) => {
                   inputRefs.current[index] = el;
                 }}
+                autoFocus={index === 0}
               />
             ))}
           </div>
+          {invalidCodeMessage && (
+            <p style={{ color: "red" }}>{invalidCodeMessage}</p>
+          )}
           <div className={styles.modalButtons}>
             {canResend ? (
               <button
@@ -251,9 +263,6 @@ const AuthConfirmCode = ({
           )}
 
           {warning && <p style={{ color: "red" }}>{warning}</p>}
-          {invalidCodeMessage && (
-            <p style={{ color: "red" }}>{invalidCodeMessage}</p>
-          )}
         </form>
       )}
     </>
