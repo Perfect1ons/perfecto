5234ff5f644eaec42aafb42092844f515e00e2d4
Эндарбек <endarashiraliev@gmail.com>
2024-07-23 09:51:45 +0600

    стандартизировал  инпуты в корзине

--- src/components/BasketComponents/BasketOrder/BasketOrder.tsx
+++ src/components/BasketComponents/BasketOrder/BasketOrder.tsx
@@ -319,69 +319,80 @@ const BasketOrder = ({ paymentMethod, deliveryMethod }: IBasketOrderProps) => {
         )}
         <div className={styles.wrap_phone}>
           <div className={styles.wrap_phone_control}>
-            <button
-              onClick={() => visibleHandler("country")}
-              className={styles.wrap_phone_control_selectCountry}
+            <InputMask
+              mask={mask}
+              value={buyer.phone}
+              onChange={handleBuyerChange}
+              className={styles.auth__input}
             >
+              {(inputProps: React.InputHTMLAttributes<HTMLInputElement>) => (
+                <input
+                  autoComplete="off"
+                  {...inputProps}
+                  name="phone"
+                  placeholder="Телефон ( Обязательно )"
+                  type="text"
+                  required
+                />
+              )}
+            </InputMask>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                visibleHandler("country");
+              }}
+              className={styles.select__country}
+            >
+              <span
+                className={cn(
+                  visible === "country"
+                    ? styles.select__country_arrow__active
+                    : styles.select__country_arrow
+                )}
+              >
+                <ArrowDropdown />
+              </span>
               <Image
-                className={styles.wrap_phone_control_selectCountry_img}
+                className={styles.select__country_img}
                 src={currentCodeCountry.img}
                 width={30}
                 height={30}
                 alt={currentCodeCountry.name}
               />
-              <span
-                className={cn(
-                  visible === "country"
-                    ? styles.wrap_phone_control_selectCountry_arrow__active
-                    : styles.wrap_phone_control_selectCountry_arrow
-                )}
-              >
-                <ArrowDropdown />
-              </span>
             </button>
-            <InputMask
-              mask={mask}
-              value={buyer.phone}
-              onChange={handleBuyerChange}
-              className={styles.wrap_phone_control_input}
-            >
-              {(inputProps: React.InputHTMLAttributes<HTMLInputElement>) => (
-                <input
-                  {...inputProps}
-                  name="phone"
-                  type="text"
-                  placeholder="Телефон"
-                />
-              )}
-            </InputMask>
           </div>
           {visible === "country" && (
             <div className={styles.wrap_phone_dropdown}>{countryOptions}</div>
           )}
         </div>
-        <div className={styles.wrap_surname}>
-          <input
-            name="surname"
-            placeholder="Фамилия"
-            type="text"
-            value={buyer.surname}
-            onChange={handleBuyerChange}
-            className={styles.wrap_surname_input}
-          />
+        <div className={styles.containerInputLabel}>
+          <div className={styles.mail__label}>
+            <input
+              className={styles.mail__inputField}
+              value={buyer.surname}
+              name="surname"
+              type="text"
+              onChange={handleBuyerChange}
+            />
+            <label className={styles.mail__inputLabel}>Фамилия</label>
+          </div>
           {surnameWarning.length > 0 && (
             <p className={styles.wrap_warning}>{surnameWarning}</p>
           )}
         </div>
-        <div className={styles.wrap_surname}>
-          <input
-            name="name"
-            placeholder="Имя"
-            type="text"
-            value={buyer.name}
-            onChange={handleBuyerChange}
-            className={styles.wrap_surname_input}
-          />
+
+        <div className={styles.containerInputLabel}>
+          <div className={styles.mail__label}>
+            <input
+              className={styles.mail__inputField}
+              value={buyer.name}
+              name="name"
+              type="text"
+              onChange={handleBuyerChange}
+            />
+            <label className={styles.mail__inputLabel}>Имя</label>
+          </div>
           {nameWarning.length > 0 && (
             <p className={styles.wrap_warning}>{nameWarning}</p>
           )}
@@ -409,16 +420,16 @@ const BasketOrder = ({ paymentMethod, deliveryMethod }: IBasketOrderProps) => {
                 : styles.wrap_organization_dropdown
             )}
           >
-            <input
-              placeholder="Название организации:"
-              className={styles.wrap_organization_dropdown__name}
-              type="text"
-            />
-            <input
-              placeholder="ИНН:"
-              className={styles.wrap_organization_dropdown__inn}
-              type="text"
-            />
+            <div className={styles.mail__label}>
+              <input className={styles.mail__inputField} type="text" />
+              <label className={styles.mail__inputLabel}>
+                Название организации:
+              </label>
+            </div>
+            <div className={styles.mail__label}>
+              <input className={styles.mail__inputField} type="text" />
+              <label className={styles.mail__inputLabel}>ИНН:</label>
+            </div>
             <div className={styles.wrap_organization_dropdown_nds}>
               <label className={styles.wrap_organization_dropdown_nds_switch}>
                 <input onClick={ndsHandler} type="checkbox" />
