ead478cbf90356bb9271078ef10a784e72102218
Ilya <ilya.yohumming@icloud.com>
2024-07-23 11:43:43 +0600

    add select time delivery modal cart

--- src/components/BasketComponents/BasketOrder/ChosingDeliveryModal/CourierDeliveryType/CourierDeliveryType.tsx
+++ src/components/BasketComponents/BasketOrder/ChosingDeliveryModal/CourierDeliveryType/CourierDeliveryType.tsx
@@ -11,6 +11,39 @@ interface ICourierDeliveryTypeProps {
   selectDelivery: (value: string) => void;
 }
 
+const times = [
+  {
+    value: "с 9.00 до 10.00",
+  },
+  {
+    value: "с 10.00 до 11.00",
+  },
+  {
+    value: "с 11.00 до 12.00",
+  },
+  {
+    value: "с 12.00 до 13.00",
+  },
+  {
+    value: "с 14.00 до 15.00",
+  },
+  {
+    value: "с 15.00 до 16.00",
+  },
+  {
+    value: "с 16.00 до 17.00",
+  },
+  {
+    value: "с 17.00 до 18.00",
+  },
+  {
+    value: "с 18.00 до 19.00",
+  },
+  {
+    value: "с 19.00 до 20.00",
+  },
+];
+
 const CourierDeliveryType = ({
   variants,
   selectDelivery,
@@ -18,8 +51,21 @@ const CourierDeliveryType = ({
 }: ICourierDeliveryTypeProps) => {
   const [openTime, setOpenTime] = useState(false);
 
+  const [openSelect, setOpenSelect] = useState(false);
+
+  const [selectedTime, setSelectedTime] = useState("Не выбрано");
+
+  const selectHandler = () => {
+    setOpenSelect(!openSelect);
+  };
+
   const openTimeHandler = () => {
     setOpenTime(!openTime);
+  };
+
+  const selectTime = (value: string) => {
+    setSelectedTime(value);
+    setOpenSelect(false);
   };
 
   return (
@@ -194,7 +240,10 @@ const CourierDeliveryType = ({
       <div className={styles.wrap_courier_selectTime}>
         <button
           onClick={openTimeHandler}
-          className={styles.wrap_courier_selectTime_button}
+          className={cn(
+            styles.wrap_courier_selectTime_button,
+            openTime && styles.wrap_courier_selectTime_button_active
+          )}
         >
           Указать время{" "}
           <span
@@ -211,7 +260,44 @@ const CourierDeliveryType = ({
             styles.wrap_courier_selectTime_dropdown,
             openTime && styles.wrap_courier_selectTime_dropdown_active
           )}
-        ></div>
+        >
+          <button
+            onClick={selectHandler}
+            className={cn(
+              styles.wrap_courier_selectTime_dropdown_toggler,
+              openSelect &&
+                styles.wrap_courier_selectTime_dropdown_toggler_active
+            )}
+          >
+            {selectedTime}
+            <span
+              className={cn(
+                styles.wrap_courier_selectTime_dropdown_toggler_arrow,
+                openSelect &&
+                  styles.wrap_courier_selectTime_dropdown_toggler_arrow_active
+              )}
+            >
+              <ArrowDropdown />
+            </span>
+          </button>
+          {openSelect && (
+            <ul className={cn(styles.wrap_courier_selectTime_dropdown_time)}>
+              {times.map((time, index) => {
+                return (
+                  <li
+                    onClick={() => selectTime(time.value)}
+                    key={index}
+                    className={
+                      styles.wrap_courier_selectTime_dropdown_time_item
+                    }
+                  >
+                    {time.value}
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
