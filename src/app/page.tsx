51923c8e1004352bfdc382fc8a30ee54f7790587
Ilya <ilya.yohumming@icloud.com>
2024-08-05 17:59:54 +0600

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ import dynamic from "next/dynamic";
 const DynamicPopularCategory = dynamic(
   () => import("@/components/HomeComponents/PopularCategory/PopularCategory"),
   {
-    ssr: false,
+    ssr: true,
     loading: () => <CategorySkeleton title="Популярные категории" />,
   }
 );
